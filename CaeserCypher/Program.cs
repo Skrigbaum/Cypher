9028d323b928cc8b102a1af25974eba794701c06
Spencer Krigbaum <skrigbaum872002@gmail.com>
2015-10-25 23:13:51 -0400

    Basic Caeser Cypher first commit

---
+++ CaeserCypher/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaeserCypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cipherText = "";
+            const int shift = 2;
+            Console.WriteLine("Please input string: ");
+            string message = Console.ReadLine();
+            message = message.ToLower();
+            int position = 0;
+            char mychar = message.ElementAt(position);
+            while (position < message.Length)
+            {
+                mychar = message.ElementAt(position);
+                if (96 + shift < mychar && mychar <= 122)
+                {
+                    mychar = Convert.ToChar(mychar - shift);
+                    cipherText = cipherText + mychar;
+                    position++;
+                }
+                else if (mychar > 96 && mychar <= 96 + shift)
+                {
+                    mychar = Convert.ToChar(mychar + 26 - shift);
+                    cipherText = cipherText + mychar;
+                    position++;
+                }
+                else Console.WriteLine("Input Text Please: ");
+                Console.WriteLine(cipherText);
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
